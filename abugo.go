d8d48b1916507bd201bda62a62323159a9be0893
abu <abu@gmail.com>
2022-12-19 15:45:51 +0800

    1

--- abugo.go
+++ abugo.go
@@ -1,7 +1,22 @@
 package abugo
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+
+	mrand "math/rand"
+
+	"github.com/beego/beego/logs"
+	"github.com/gin-gonic/gin"
+)
 
 func Init() {
-	fmt.Println("abugo init 1")
+	mrand.Seed(time.Now().Unix())
+	gin.SetMode(gin.ReleaseMode)
+	logs.EnableFuncCallDepth(true)
+	logs.SetLogFuncCallDepth(3)
+	logs.SetLogger(logs.AdapterFile, `{"filename":"_log/logfile.log","maxsize":10485760}`)
+	logs.SetLogger(logs.AdapterConsole, `{"color":true}`)
+
+	fmt.Println("abugo init v1.0.3")
 }
