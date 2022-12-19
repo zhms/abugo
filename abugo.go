5cc5550ae2bff4c49254dfacc6dec1d984d042e0
abu <abu@gmail.com>
2022-12-19 21:27:55 +0800

    'auto'

--- abugo.go
+++ abugo.go
@@ -1,6 +1,8 @@
 package abugo
 
 import (
+	"fmt"
+	"strconv"
 	"time"
 
 	mrand "math/rand"
@@ -29,4 +31,125 @@ func Init() {
 	if nodeid > 0 {
 		NewIdWorker(nodeid)
 	}
+}
+
+func GetStringFromInterface(v interface{}) string {
+	if v == nil {
+		return ""
+	}
+	switch v.(type) {
+	case string:
+		return v.(string)
+	case int:
+		return fmt.Sprint(v.(int))
+	case int32:
+		return fmt.Sprint(v.(int32))
+	case int64:
+		return fmt.Sprint(v.(int64))
+	case float32:
+		return fmt.Sprint(v.(float32))
+	case float64:
+		return fmt.Sprint(v.(float64))
+	}
+	return ""
+}
+
+func GetInt64FromInterface(v interface{}) int64 {
+	if v == nil {
+		return 0
+	}
+	switch v.(type) {
+	case string:
+		i, err := strconv.ParseInt(v.(string), 10, 64)
+		if err != nil {
+			return 0
+		}
+		return i
+	case int:
+		return int64(v.(int))
+	case int32:
+		return int64(v.(int32))
+	case int64:
+		return int64(v.(int64))
+	case float32:
+		return int64(v.(float32))
+	case float64:
+		return int64(v.(float64))
+	}
+	return 0
+}
+
+func GetIntFromInterface(v interface{}) int32 {
+	if v == nil {
+		return 0
+	}
+	switch v.(type) {
+	case string:
+		i, err := strconv.ParseInt(v.(string), 10, 64)
+		if err != nil {
+			return 0
+		}
+		return int32(i)
+	case int:
+		return int32(v.(int))
+	case int32:
+		return int32(v.(int32))
+	case int64:
+		return int32(v.(int64))
+	case float32:
+		return int32(v.(float32))
+	case float64:
+		return int32(v.(float64))
+	}
+	return 0
+}
+
+func GetFloat64FromInterface(v interface{}) float64 {
+	if v == nil {
+		return 0
+	}
+	switch v.(type) {
+	case string:
+		i, err := strconv.ParseFloat(v.(string), 64)
+		if err != nil {
+			return 0
+		}
+		return i
+	case int:
+		return float64(v.(int))
+	case int32:
+		return float64(v.(int32))
+	case int64:
+		return float64(v.(int64))
+	case float32:
+		return float64(v.(float32))
+	case float64:
+		return v.(float64)
+	}
+	return 0
+}
+
+func GetFloatFromInterface(v interface{}) float32 {
+	if v == nil {
+		return 0
+	}
+	switch v.(type) {
+	case string:
+		i, err := strconv.ParseFloat(v.(string), 64)
+		if err != nil {
+			return 0
+		}
+		return float32(i)
+	case int:
+		return float32(v.(int))
+	case int32:
+		return float32(v.(int32))
+	case int64:
+		return float32(v.(int64))
+	case float32:
+		return float32(v.(float32))
+	case float64:
+		return float32(v.(float64))
+	}
+	return 0
 }
