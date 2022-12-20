bf296fae407918be08347b2c60eae6fd7b61e4aa
abu <abu@gmail.com>
2022-12-20 12:41:59 +0800

    'auto'

--- aburedis.go
+++ aburedis.go
@@ -1,7 +1,9 @@
 package abugo
 
 import (
+	"encoding/json"
 	"fmt"
+	"reflect"
 	"sync"
 	"time"
 
@@ -64,4 +66,198 @@ func (c *AbuRedis) Init(prefix string) {
 	c.subscribecallbacks = make(map[string]AbuRedisSubCallback)
 	c.mu = new(sync.RWMutex)
 	logs.Debug("连接redis 成功:", host, port, db)
+}
+
+func (c *AbuRedis) getcallback(channel string) AbuRedisSubCallback {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	return c.subscribecallbacks[channel]
+}
+
+func (c *AbuRedis) subscribe(channels ...string) {
+	c.pubconnection.Subscribe(redis.Args{}.AddFlat(channels)...)
+	if !c.recving {
+		go func() {
+			for {
+				imsg := c.pubconnection.Receive()
+				msgtype := reflect.TypeOf(imsg).Name()
+				if msgtype == "Message" {
+					msg := imsg.(redis.Message)
+					callback := c.getcallback(msg.Channel)
+					if callback != nil {
+						callback(string(msg.Data))
+					}
+				}
+			}
+		}()
+	}
+}
+
+func (c *AbuRedis) Subscribe(channel string, callback AbuRedisSubCallback) {
+	c.mu.Lock()
+	c.subscribecallbacks[channel] = callback
+	c.mu.Unlock()
+	c.subscribe(channel)
+}
+
+func (c *AbuRedis) Publish(k, v interface{}) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	output, _ := json.Marshal(&v)
+	_, err := conn.Do("publish", k, output)
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) Get(k string) interface{} {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	ret, err := conn.Do("get", k)
+	if err != nil {
+		logs.Error(err.Error())
+		return nil
+	}
+	return ret
+}
+
+func (c *AbuRedis) Set(k string, v interface{}) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	output, _ := json.Marshal(&v)
+	_, err := conn.Do("set", k, output)
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) SetString(k string, v string) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	_, err := conn.Do("set", k, v)
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) SetEx(k string, to int, v interface{}) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	output, _ := json.Marshal(&v)
+	_, err := conn.Do("setex", k, to, string(output))
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+func (c *AbuRedis) SetStringEx(k string, to int, v string) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	_, err := conn.Do("setex", k, to, v)
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) Del(k string) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	_, err := conn.Do("del", k)
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) Expire(k string, to int) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	_, err := conn.Do("expire", k, to)
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) HSet(k string, f string, v interface{}) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	output, _ := json.Marshal(&v)
+	_, err := conn.Do("hset", k, f, string(output))
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) HSetString(k string, f string, v string) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	_, err := conn.Do("hset", k, f, v)
+	if err != nil {
+		logs.Error(err.Error())
+		return err
+	}
+	return nil
+}
+
+func (c *AbuRedis) HGet(k string, f string) interface{} {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	ret, err := conn.Do("hget", k, f)
+	if err != nil {
+		logs.Error(err.Error())
+		return nil
+	}
+	return ret
+}
+
+func (c *AbuRedis) HDel(k string, f string) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	_, err := conn.Do("hdel", k, f)
+	if err != nil {
+		logs.Error(err.Error())
+		return nil
+	}
+	return nil
+}
+
+func (c *AbuRedis) HKeys(k string) []string {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	keys, err := conn.Do("hkeys", k)
+	ikeys := keys.([]interface{})
+	strkeys := []string{}
+	if err != nil {
+		logs.Error(err.Error())
+		return strkeys
+	}
+	for i := 0; i < len(ikeys); i++ {
+		strkeys = append(strkeys, string(ikeys[i].([]byte)))
+	}
+	return strkeys
+}
+
+func (c *AbuRedis) SAdd(k string, f string) error {
+	conn := c.redispool.Get()
+	defer conn.Close()
+	_, err := conn.Do("sadd", k, f)
+	if err != nil {
+		logs.Error(err.Error())
+		return nil
+	}
+	return nil
 }
