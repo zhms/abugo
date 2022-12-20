1710eff9efb9a8a8f9bd031853b4113ee8356069
abu <abu@gmail.com>
2022-12-20 15:26:52 +0800

    'auto'

--- abuhttp.go
+++ abuhttp.go
@@ -1,13 +1,16 @@
 package abugo
 
 import (
+	"encoding/json"
 	"fmt"
 	"mime/multipart"
 	"net/http"
+	"sync"
 
 	"github.com/beego/beego/logs"
 	"github.com/gin-gonic/gin"
 	val "github.com/go-playground/validator/v10"
+	"github.com/gorilla/websocket"
 	"github.com/spf13/viper"
 )
 
@@ -74,6 +77,14 @@ type AbuHttp struct {
 	token         *AbuRedis
 	tokenrefix    string
 	tokenlifetime int
+
+	upgrader         websocket.Upgrader
+	idx_conn         sync.Map
+	conn_idx         sync.Map
+	connect_callback AbuWsCallback
+	close_callback   AbuWsCallback
+	msgtype          sync.Map
+	msg_callback     sync.Map
 }
 
 type AbuHttpGropu struct {
@@ -231,6 +242,12 @@ func (c *AbuHttp) Init(cfgkey string) {
 		bind := fmt.Sprint("0.0.0.0:", port)
 		c.gin.Run(bind)
 	}()
+	c.upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+	c.GetNoAuth("/api/ws", c.ws)
 	logs.Debug("http listen:", port)
 }
 
@@ -325,4 +342,128 @@ func (c *AbuHttp) RenewToken(key string) {
 		return
 	}
 	c.token.Expire(fmt.Sprint(c.tokenrefix, ":", key), c.tokenlifetime)
+}
+
+type AbuWsCallback func(int64)
+type AbuWsMsgCallback func(int64, interface{})
+type abumsgdata struct {
+	MsgId string      `json:"msgid"`
+	Data  interface{} `json:"data"`
+}
+
+func (c *AbuHttp) ws(ctx *AbuHttpContent) {
+	conn, err := c.upgrader.Upgrade(ctx.Gin().Writer, ctx.Gin().Request, nil)
+	if err != nil {
+		logs.Error(err)
+		return
+	}
+	defer conn.Close()
+	id := AbuId()
+	c.idx_conn.Store(id, conn)
+	c.conn_idx.Store(conn, id)
+	if c.connect_callback != nil {
+		c.connect_callback(id)
+	}
+	for {
+		mt, message, err := conn.ReadMessage()
+		c.msgtype.Store(id, mt)
+		if err != nil {
+			break
+		}
+		md := abumsgdata{}
+		err = json.Unmarshal(message, &md)
+		if err == nil {
+			callback, cbok := c.msg_callback.Load(md.MsgId)
+			if cbok {
+				cb := callback.(AbuWsMsgCallback)
+				cb(id, md.Data)
+			}
+		}
+	}
+	_, ccerr := c.idx_conn.Load(id)
+	if ccerr {
+		c.idx_conn.Delete(id)
+		c.conn_idx.Delete(conn)
+		if c.close_callback != nil {
+			c.close_callback(id)
+		}
+	}
+}
+
+func (c *AbuHttp) WsSendMsg(id int64, msgid string, data interface{}) {
+	iconn, connok := c.idx_conn.Load(id)
+	imt, mtok := c.msgtype.Load(id)
+	if mtok && connok {
+		conn := iconn.(*websocket.Conn)
+		mt := imt.(int)
+		msg := abumsgdata{msgid, data}
+		msgbyte, jerr := json.Marshal(msg)
+		if jerr == nil {
+			werr := conn.WriteMessage(mt, msgbyte)
+			if werr != nil {
+			}
+		}
+	}
+}
+
+func (c *AbuHttp) WsClose(id int64) {
+	iconn, connok := c.idx_conn.Load(id)
+	if connok {
+		conn := iconn.(*websocket.Conn)
+		c.conn_idx.Delete(conn)
+		c.idx_conn.Delete(id)
+		c.msgtype.Delete(id)
+		conn.Close()
+	}
+}
+
+func (c *AbuHttp) WsAddConnectCallback(callback AbuWsCallback) {
+	c.connect_callback = callback
+}
+
+func (c *AbuHttp) WsAddMsgCallback(msgid string, callback AbuWsMsgCallback) {
+	c.msg_callback.Store(msgid, callback)
+}
+
+func (c *AbuHttp) WsAddCloseCallback(callback AbuWsCallback) {
+	c.close_callback = callback
+}
+
+func (c *AbuHttp) WsConnect(host string, callback AbuWsCallback) {
+	go func() {
+		conn, _, err := websocket.DefaultDialer.Dial(host, nil)
+		if err != nil {
+			callback(0)
+			return
+		}
+		defer conn.Close()
+		id := AbuId()
+		c.idx_conn.Store(id, conn)
+		c.conn_idx.Store(conn, id)
+		callback(id)
+		for {
+			mt, message, err := conn.ReadMessage()
+			c.msgtype.Store(id, mt)
+			if err != nil {
+				break
+			}
+			md := abumsgdata{}
+			err = json.Unmarshal(message, &md)
+			if err == nil {
+				callback, cbok := c.msg_callback.Load(md.MsgId)
+				if cbok {
+					cb := callback.(AbuWsMsgCallback)
+					cb(id, md.Data)
+				}
+			}
+		}
+		_, ccerr := c.idx_conn.Load(id)
+		if ccerr {
+			c.idx_conn.Delete(id)
+			c.conn_idx.Delete(conn)
+			if c.close_callback != nil {
+				c.close_callback(id)
+			}
+		}
+	}()
 }
